56e46486bfb6b91da980d66e5fa5720cc78bd768
Eduardo F. Durand Obando <122226117+EddieCodder@users.noreply.github.com>
2023-11-13 17:21:02 -0500

    Conectando LoginScreen a RegisterScreen

--- lib/pantallas/login.dart
+++ lib/pantallas/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medic_app/pantallas/register.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -151,6 +152,12 @@ class LoginScreen extends StatelessWidget {
                       TextButton(
                         onPressed: () {
                           // TODO: Redireccionar a RegisterScreen 
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const RegisterScreen(),
+                            ),
+                          );                          
                         },
                         child: const Text('Regístrate', style: TextStyle(color: Color(0xFF471AA0), fontWeight: FontWeight.bold),)
                       )
