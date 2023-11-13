871e1b04b018b24fbcac9f02f86e499fb8719047
EddieCodder <ed.durand.obando@gmail.com>
2023-11-13 16:45:58 -0500

    Pantalla de inicio sesión terminada

--- lib/pantallas/login.dart
+++ lib/pantallas/login.dart
@@ -5,66 +5,218 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size; // Tamaño de la pantalla del celular
+    final size =
+        MediaQuery.of(context).size; // Tamaño de la pantalla del celular
 
     return Scaffold(
       body: Container(
         width: double.infinity, // Desde el principio de la pantalla
         height: double.infinity, // Desde el principio de la pantalla
         color: const Color.fromARGB(255, 255, 255, 255),
-        child: Stack(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
           // Conjunto de widgets
           children: [
+            // Espacio
+            const SizedBox(
+              height: 150,
+            ),
+
             // Icono Persona - Logo de la App
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround, // Organiza de manera uniforme a los iconos
+              mainAxisAlignment: MainAxisAlignment
+                  .spaceAround, // Organiza de manera uniforme a los iconos
               children: [
+                // Logo de la persona
                 Image.asset(
                   'assets/logoPersona.png',
                 ),
 
+                // Logo de la app
                 Image.asset(
                   'assets/logoApp.png',
+                  height: 150,
                 ),
-
-
-
               ],
             ),
+
+            // Espacio
+            SizedBox(height: size.height * 0.02),
+
             // Texto iniciar Sesión
-            Positioned(
-              top: size.height * 0.4, // Ajuste la posición según sea necesario
-              left: size.width * 0.120,
-              child: const Text(
-                'Iniciar sesión',
-                style: TextStyle(
-                  color: Color(0xFF471AA0),
-                  fontSize: 30,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w700,
-                ),
+            Padding(
+              padding: EdgeInsets.all(size.width * 0.1),
+              child: Column(
+                children: [
+                  
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Iniciar sesión',
+                        style: TextStyle(
+                          color: Color(0xFF471AA0),
+                          fontSize: 30,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  // Campo de texto Usuario
+                  SizedBox(height: size.height * 0.02),
+
+                  const TextFieldRegister(
+                    hintText: 'Nombre de Usuario o Correo',
+                  ),
+                  
+                  // Campo de texto Usuario
+                  SizedBox(height: size.height * 0.02),
+                  const TextFieldRegister(
+                    hintText: 'Contraseña',
+                    iconRight: Icon(Icons.remove_red_eye_outlined),
+                  ),       
+
+                  // Boton textual olvidaste contraseñas
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          // TODO:Redireccionar a LoginScreen
+                        },
+                        child: const Text('Olvidaste tu contraseña', style: TextStyle(color: Color(0xFF471AA0), fontWeight: FontWeight.bold),)
+                      ),
+                      ]
+                  ),
+
+                  SizedBox(height: size.height * 0.02),
+
+                  FloatingActionButton.extended(
+                      onPressed: () {
+                        // TODO: Realizar operacion de Login
+                      },
+                      label: const Text('Iniciar Sesión'),
+                      extendedPadding:EdgeInsets.symmetric(horizontal: size.width),
+                      backgroundColor: const Color(0xFF9550CE),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15), 
+                      )
+                  ),
+
+
+                  SizedBox(height: size.height * 0.04),
+                  const Text('O ingresa con', style: TextStyle(color: Color(0xFF471AA0))),
+
+                  SizedBox(height: size.height * 0.02),
+
+                  // Botones de Google Facebook Twitter
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      // Google
+                      TextButton(
+                        onPressed: () {
+                          //TODO: INICIAR SESIÓN CON GOOGLE
+                        }, 
+                        child: Image.asset('assets/logoGoogle.png')
+                      ),
+
+                      // Facebook
+                      TextButton(
+                        onPressed: () {
+                          //TODO: INICIAR SESIÓN CON FACEBOOK
+                        }, 
+                        child: Image.asset('assets/logoFacebook.png')
+                      ),
+
+                      // Twitter
+                      TextButton(
+                        onPressed: () {
+                          //TODO: INICIAR SESIÓN CON TWITTER
+                        }, 
+                        child: Image.asset('assets/logoTwitter.png')
+                      ),
+                    ],
+
+                  ),
+                  
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('No tienes una cuenta', style: TextStyle(color: Color(0xFF471AA0))),
+                      TextButton(
+                        onPressed: () {
+                          // TODO: Redireccionar a RegisterScreen 
+                        },
+                        child: const Text('Regístrate', style: TextStyle(color: Color(0xFF471AA0), fontWeight: FontWeight.bold),)
+                      )
+                    ],
+                  ),                  
+                ],
               ),
             ),
-            
-            /*Container(
-                width: 390,
-                height: 50,
-                decoration: ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                        side: BorderSide(width: 2, color: Color(0xFF9747FF)),
-                        borderRadius: BorderRadius.circular(15),
-                    ),
-                ),
-            )*/
-
-
           ],
         ),
       ),
     );
-
-
   }
 }
-    
 
+class TextFieldRegister extends StatefulWidget {
+  final String hintText;
+  final Icon? iconRight;
+
+  const TextFieldRegister({
+    Key? key,
+    required this.hintText,
+    this.iconRight,
+  }) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _TextFieldRegisterState createState() => _TextFieldRegisterState();
+}
+
+class _TextFieldRegisterState extends State<TextFieldRegister> {
+  //late TextEditingController _controller;
+  bool textoOculto = false; // Oculta el texto
+  final outlineInputBorder = OutlineInputBorder(
+      borderSide: const BorderSide(width: 2, color: Color(0xFF9747FF)),
+      borderRadius: BorderRadius.circular(15));
+
+  @override
+  void initState() {
+    super.initState();
+    //_controller = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      //controller: _controller,
+      obscureText: textoOculto,
+      decoration: InputDecoration(
+        focusedBorder: outlineInputBorder,
+        enabledBorder: outlineInputBorder,
+        hintText: widget.hintText,
+        suffixIcon: validarIconoDerecha(widget.iconRight),
+      ),
+    );
+  }
+
+  Widget? validarIconoDerecha(Icon? iconRight) {
+    if (iconRight == null) return null;
+    return IconButton(
+      onPressed: () {
+        setState(() {
+          textoOculto = !textoOculto;
+        });
+      },
+      icon: iconRight,
+    );
+  }
+}
